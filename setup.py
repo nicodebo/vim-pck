c54f6dc1adb6dd5f13764843e26cf6296a291833
nicodebo <nico.debo@openmailbox.org>
2017-03-08 18:31:53 +0100

    test raise version setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'sh', 'tqdm']
 
 setup(
     name='vimpck',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/nicodebo/vim-pck',
     license='BSD',
     author='Nicolas D.',
@@ -26,22 +26,22 @@ setup(
     },
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        # 'Development Status :: 1 - Planning',
+        'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
+        # 'Operating System :: POSIX',
+        # 'Operating System :: MacOS',
         'Operating System :: Unix',
-        'Operating System :: Microsoft :: Windows',
+        # 'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
