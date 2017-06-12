f32edc0597e56d6944fac44752fdbab1d6d7543c
nicodebo <nico.debo@openmailbox.org>
2017-06-12 15:28:24 +0200

    Add Parser Class
    - Parser class allow to translate in a text, key value pair of
    dictionary. The key is the ansi escape sequence representation in the
    string while the value is the real ansi escape sequence.

--- vim_pck/ansi.py
+++ vim_pck/ansi.py
@@ -3,10 +3,12 @@ Ansi escape sequence module
 Ref: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
 """
 
+import re
 from enum import Enum
 
 
 class EscapeSeq(Enum):
+    """Base class for the ansi escape sequence Enumeration"""
 
     @staticmethod
     def reset():
@@ -18,9 +20,14 @@ class EscapeSeq(Enum):
         return "\u001b[0m"
 
     def describe(self):
+        """Return the name and the value of the current Enum"""
         return self.name, self.value
 
     def enclose(self, string):
+        """Enclose with the current Enum value on the left and ansi reset on
+        the right
+        """
+
         escaped_string = "{}{}{}".format(self.describe()[1],
                                          string,
                                          self.reset())
@@ -58,8 +65,78 @@ class Decoration(EscapeSeq):
     reverse = "\u001b[7m"
 
 
+class Parser:
+    """Search and replace code with escape sequence
+
+    Attributs:
+            - lhs (str): left hand side
+            - rhs (str): right hand side
+            - ansi_enums (list(object instance)): list of enumerations
+              defining the escape sequence
+
+
+    There is hard containers/contains relashionship between this class and the
+    enum classes.
+
+    ex:
+    "<bold>some text<reset>"
+    the sub method parse and replace with the corresponding ansi escape
+    sequence --> "\u001b[1msome text\u001b[0m"
+    The lhs and rhs allow to customize the way the parsed ansi are
+    represented. For exemple "[bold]some text[reset]" with lhs = '[' and
+    rhs = ']'
+
+    """
+
+    def __init__(self, lhs='<', rhs='>'):
+        self.lhs = lhs
+        self.rhs = rhs
+        self.ansi_enums = list(Colors16) + list(Decoration)
+        self.ansi_corr = self.create_table()
+
+    def wrap(self, string):
+        """wrap string between lhs and rhs"""
+        return "{0}{1}{2}".format(self.lhs, string, self.rhs)
+
+    def create_table(self):
+        """ Create a correspondance table between ansi representation and the
+        real escape sequence
+        ex:
+        if lhs and rhs are set to default values
+            | <red>  | \u001b[1m |
+            | <bold> | \u001b[0m |
+            | ...    | ...       |
+        """
+        ansi_corr = {}  # Ansi correspondance dictionnary initialization
+        ansi_corr[self.wrap('reset')] = EscapeSeq.reset()  # map reset
+        # map Colors16 and Decoration values
+        for elem in self.ansi_enums:
+            ansi_corr[self.wrap(elem.name)] = elem.value
+        return ansi_corr
+
+    def sub(self, text):
+        """ Replace in 'text' all occurences of any key in the given
+        dictionary by its corresponding value. Returns the new string.
+
+        http://code.activestate.com/recipes/81330-single-pass-multiple-replace/
+        """
+
+        # Create a regular expression from the dictionary keys
+        regex = re.compile("(%s)" % "|".join(map(re.escape,
+                                                 self.ansi_corr.keys())))
+
+        # For each match, look-up corresponding value in dictionary
+        return regex.sub(lambda mo: self.ansi_corr[mo.string[mo.start():mo.end()]], text)
+
+    def __del__(self):
+        # make sure to reset the terminal state back to default
+        # does not seem to matter for zsh but important for bash
+        print(EscapeSeq.reset())
+
+
 # Quick exemple usage
 if __name__ == "__main__":
+    # 1st method: with the enum method 'enclose'
     for color in Colors16:
         name, ansi = color.describe()
         if name[0].islower():
@@ -70,4 +147,10 @@ if __name__ == "__main__":
     for deco in Decoration:
         name, ansi = deco.describe()
         print(deco.enclose("some {} text".format(name)))
+
+    # 2nd method: with the Parser class
+    ansi_parser = Parser()
+    print(ansi_parser.ansi_corr)
+    ex_text = "<underline><red>some text<reset>"
+    print(ansi_parser.sub(ansi_parser.ansi_corr, ex_text))
 
